ad6d9da77de45f326b77d4ebe2f0c006c8440a41
Jesus Kauze <12-10273@usb.ve>
2017-12-21 11:49:33 -0400

    Pagina inicio v1.0
    Caracteristicas y textos actualizados
    Agregado boton info en el Nav

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,17 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
   }
+  
+//Funcion de Alerts para la muestra del mensaje de informacion de la aplicacion
+  MostrarInfo() {
+    let alert = this.alertCtrl.create({
+      title: 'Informaciones',
+      subTitle: 'Esta aplicacion tuvo como proposito principal la implementacion de local-notifications, por problemas del android-sdk en conjunto con incompatiblidades entre librerias de Linux (LM 16.08) fue imposible probar dichas funcionalidades, por lo que quedo descartado las locaNotifications',
+      buttons: ['Entendido']
+    });
+    alert.present();
+  }  
 
 }
